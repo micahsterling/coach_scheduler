1a23db699e0cf8125c5ad08041acc4aa9db18445
Micah Sterling <msterling89@gmail.com>
2021-09-01 09:58:47 -0500

    add show action/route

--- app/controllers/api/coaches_controller.rb
+++ app/controllers/api/coaches_controller.rb
@@ -1,6 +1,39 @@
 class Api::CoachesController < ApplicationController
   def index
-    @message = "hello"
+    @coaches = [
+        "Christy Schumm",
+        "Natalia Stanton Jr.",
+        "Nola Murazik V","Elyssa O'Kon",
+        "Dr. Geovany Keebler"
+      ]
     render "index.json.jb"
+  end
+
+  def show 
+    @appointment = {
+      "Christy Schumm":{
+        "Monday": {
+          "9:00 AM": true,
+          "9:30 AM": true,
+          "10:00 AM": true,
+          "10:30 AM": true,
+          "11:00 AM": true,
+          "11:30 AM": true,
+          "12:00 PM": true,
+          "12:30 PM": true,
+          "1:00 PM": true,
+          "1:30 PM": true,
+          "2:00 PM": true,
+          "2:30 PM": true,
+          "3:00 PM": true,
+          "3:30 PM": true,
+          "4:00 PM": true,
+          "4:30 PM": true,
+          "5:00 PM": true,
+          "5:30 PM": true,
+        }
+      }
+    }
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   namespace :api do
     get "/coaches" => "coaches#index"
+    get "/coaches/:id" => "coaches#show"
   end
 end
